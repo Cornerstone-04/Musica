34d5cc4512f4569f73686609d4e81a9ccc551efb
Cornerstone <fortunecornerstone@gmail.com>
2022-10-09 16:52:48 +0100

    Add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    // "./index.html",
+    "./main.js",
+    "./public/**/*.{html,js}",
+    "./script/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
